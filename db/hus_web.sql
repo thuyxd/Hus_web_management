5a52a8ca49adb04650df160fafc5798a0bcd7542
Thủy <38624605+thuyxd@users.noreply.github.com>
2019-11-04 09:38:35 +0700

    Add files via upload

--- db/hus_web.sql
+++ db/hus_web.sql
@@ -121,17 +121,12 @@ CREATE TABLE `intern_profile` (
 DROP TABLE IF EXISTS `organization_profile`;
 CREATE TABLE `organization_profile` (
   `id` int(11) NOT NULL,
-  `username` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `name_organization` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `employee_count` int(11) NOT NULL,
   `gross_revenue` int(11) NOT NULL,
   `address` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL,
   `contact` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `tax_number` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  `password` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL,
-  
-
+  `tax_number` varchar(20) COLLATE utf8mb4_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
 
 --
